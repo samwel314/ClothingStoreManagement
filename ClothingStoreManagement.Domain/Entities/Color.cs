f9195b0020f07816c631c8a6443dd4b8a7bab8e3
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-17 16:49:43 +0200

    refactor: color model

--- ClothingStoreManagement.Domain/Entities/Color.cs
+++ ClothingStoreManagement.Domain/Entities/Color.cs
@@ -1,17 +1,31 @@
-﻿namespace ClothingStoreManagement.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ClothingStoreManagement.Domain.Entities
 {
     public class Color
     {
         public int Id { get; private set; }
-        public string Name { get; private set; } = null!; 
-        public string Code { get; private set; } = null!; 
-        private Color() { } // EF Core
-        public Color(string name, string code)
+        public string Name { get; private set; } = null!;
+        public string Code { get; private set; } = null!;
+        public void Update(string name, string code)
+        {
+            Validate(name, code);
+            Name = name.Trim();
+            Code = code.Trim().ToUpper();
+        }
+        void Validate(string name, string code)
         {
             if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name is required");
             if (string.IsNullOrWhiteSpace(code)) throw new ArgumentException("Code is required");
-            Name = name;
-            Code = code.ToUpper();
+        }
+
+        private Color() { } // EF Core
+        public Color(string name, string code)
+        {
+            Validate(name, code);
+            Name = name.Trim();
+            Code = code.Trim().ToUpper();
         }
     }
 }
