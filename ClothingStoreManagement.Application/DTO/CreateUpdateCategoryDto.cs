1e6c2878f05a8c4209d7e43df6730897d9cdc4dc
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-19 20:20:35 +0200

    feat(category): add category dtos

---
+++ ClothingStoreManagement.Application/DTO/CreateUpdateCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClothingStoreManagement.Application.DTO
+{
+    public class CreateUpdateCategoryDto
+    {
+        [MaxLength (100 , ErrorMessage = "اقصي عدد من الحروف هو 100 حرف ") ]
+        [Required (ErrorMessage = "ادخل اسم الفئة")]
+        public string Name { get; set; } = null!; 
+        public int ? CategoryId { get; set; }   
+    }
+}
