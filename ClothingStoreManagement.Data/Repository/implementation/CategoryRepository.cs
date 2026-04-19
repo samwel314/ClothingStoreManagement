bac7918abb5012feb38f9f4fe41e080862606c29
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-19 20:16:03 +0200

    feat(category): add category repo and  unit of work

---
+++ ClothingStoreManagement.Data/Repository/implementation/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿
+using ClothingStoreManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothingStoreManagement.Data.Repository.implementation
+{
+    public class CategoryRepository : BaseRepository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+           _db = db;    
+        }
+    }
+}
--- ClothingStoreManagement.Data/Repository/implementation/UnitOfWork.cs
+++ ClothingStoreManagement.Data/Repository/implementation/UnitOfWork.cs
@@ -12,12 +12,14 @@ namespace ClothingStoreManagement.Data.Repository.implementation
         {
             _db = db;
             Colors = new ColorRepository(_db);
-            Sizes = new SizeRepository(_db);        
+            Sizes = new SizeRepository(_db);   
+            Categories = new CategoryRepository(_db);
         }
 
         public IColorRepository Colors { get; private set; }
 
         public ISizeRepository Sizes { get; private set; }
+        public ICategoryRepository Categories { get; private set; }
 
         public async Task Save()
         {
