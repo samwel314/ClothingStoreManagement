a8e46631c3080d932dfc46aeafccfd14b25a4ed2
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-19 18:48:44 +0200

    feat(Size): implement size logic operation

---
+++ ClothingStoreManagement.Application/DTO/CreateUpdateSizeDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClothingStoreManagement.Application.DTO
+{
+    public class CreateUpdateSizeDto
+    {
+        public int? Id { get; set; }
+        [MaxLength(50, ErrorMessage = "أقصى عدد حروف هو 50 حرف")]
+        [Required(ErrorMessage = "ادخل اسم المقاس")]
+        public string Name { get; set; } = null!; // مثال: Large
+
+        [MaxLength(10, ErrorMessage = "أقصى عدد حروف لكود المقاس هو 10 أحرف")]
+        [Required(ErrorMessage = "ادخل كود المقاس")]
+        public string Code { get; set; } = null!; // مثال: L
+    }
+
+    public class SizeListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Code { get; set; } = null!;
+    }
+}
