b7d25938ba7ac61651f143e261a58062160104bb
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-21 00:26:30 +0200

    refactor : remove data anoution to add fleun api

--- ClothingStoreManagement.Application/DTO/CreateProductVariantDto.cs
+++ ClothingStoreManagement.Application/DTO/CreateProductVariantDto.cs
@@ -4,15 +4,10 @@ namespace ClothingStoreManagement.Application.DTO
 {
     public class CreateProductVariantDto
     {
-        [Required(ErrorMessage = "المقاس مطلوب")]
         public int SizeId { get; set; }
-        [Required(ErrorMessage = "اللون مطلوب")]
         public int ColorId { get; set; }
-        [Range(1, int.MaxValue, ErrorMessage = "الكمية لا يمكن أن تكون اقل من صفر ")]
         public int StockQuantity { get; set; }
-        [Range(0.01, double.MaxValue, ErrorMessage = "سعر البيع يجب أن يكون أكبر من صفر")]
         public decimal SellingPrice { get; set; }
-        [Range(0.01, double.MaxValue, ErrorMessage = "سعر الشراء يجب أن يكون أكبر من صفر")]
         public decimal PurchasePrice { get; set; }
     }
 }
--- ClothingStoreManagement.Application/DTO/CreateProductWithVariantsDto.cs
+++ ClothingStoreManagement.Application/DTO/CreateProductWithVariantsDto.cs
@@ -7,14 +7,8 @@ namespace ClothingStoreManagement.Application.DTO
 {
     public class CreateProductWithVariantsDto
     {
-        [Required(ErrorMessage = "اسم المنتج مطلوب")]
-        [StringLength(100, MinimumLength = 2, ErrorMessage = "اسم المنتج يجب أن يكون بين 2 و 100 حرف")]
         public string Name { get; set; } = null!;
-        [Required(ErrorMessage = "كود المنتج (SKU) مطلوب")]
-        [StringLength(100, MinimumLength = 4, ErrorMessage = "كود المنتج يجب أن يكون بين 4 و 100 حرف")]
         public string SKU { get; set; } = null!;
-        [Required(ErrorMessage = "يجب اختيار القسم")]
-        [Range(1, int.MaxValue, ErrorMessage = "القسم المختار غير صحيح")]
         public int CategoryId { get; set; }
         public List<CreateProductVariantDto> Variants { get; set; } = new ();
     }
