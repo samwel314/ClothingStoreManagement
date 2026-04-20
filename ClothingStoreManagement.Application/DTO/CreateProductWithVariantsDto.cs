e513cc9475675ef66b657a25c3c30213279fef70
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-20 20:51:17 +0200

    feat(product): add create product dtos

---
+++ ClothingStoreManagement.Application/DTO/CreateProductWithVariantsDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ClothingStoreManagement.Application.DTO
+{
+    public class CreateProductWithVariantsDto
+    {
+        [Required(ErrorMessage = "اسم المنتج مطلوب")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "اسم المنتج يجب أن يكون بين 2 و 100 حرف")]
+        public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "كود المنتج (SKU) مطلوب")]
+        [StringLength(100, MinimumLength = 4, ErrorMessage = "كود المنتج يجب أن يكون بين 4 و 100 حرف")]
+        public string SKU { get; set; } = null!;
+        [Required(ErrorMessage = "يجب اختيار القسم")]
+        [Range(1, int.MaxValue, ErrorMessage = "القسم المختار غير صحيح")]
+        public int CategoryId { get; set; }
+        public List<CreateProductVariantDto> Variants { get; set; } = new ();
+    }
+}
