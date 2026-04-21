559ad79705ab6cc40d07a77153cfd9720d78cfbf
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-21 16:08:58 +0200

    feat(dtos): impement manage prorducst dtos

---
+++ ClothingStoreManagement.Application/DTO/ProductVariantListDto.cs
@@ -0,0 +1,13 @@
+﻿namespace ClothingStoreManagement.Application.DTO
+{
+    public class ProductVariantListDto
+    {
+        public Guid Id { get; set; }
+        public string Size { get; set; } = null!;// code 
+        public string Color { get; set; } = null!; //code 
+        public decimal Price { get; set; } // بيع 
+        public int StockQuantity { get; set; }
+        public string Sku { get; set; } = null!; 
+
+    }
+}
