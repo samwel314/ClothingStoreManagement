dd5dc475a091c3ff577b5ea0c5cdf683a4b0bf41
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-04-21 19:17:31 +0200

    feat(product):implement product filters ui

--- ClothingStoreManagement.Application/Services/ProductService.cs
+++ ClothingStoreManagement.Application/Services/ProductService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using ClothingStoreManagement.Application.DTO;
 using ClothingStoreManagement.Application.ResultHelpers;
 using ClothingStoreManagement.Application.Validation;
@@ -64,6 +65,48 @@ namespace ClothingStoreManagement.Application.Services
             await _db.Products.CreateAsync(Product);
             await _db.Save();
             return Result<string>.Success("تم إضافة المنتج بنجاح ");
+        }
+
+        public async Task<Result<Pagination<ProductListDto>>> 
+            GetAllProductsAsync(int page, int pageSize = 5, bool? active = null, int? categoryId = null, 
+            string? searchTerm = null)
+        {
+            var baseQuery = _db.Products.GetAll();
+
+            if (categoryId != null)
+                baseQuery = baseQuery.Where(p => p.CategoryId == categoryId);
+
+            if (active != null)
+                baseQuery = baseQuery.Where(p => p.IsActive == active);
+            if (searchTerm != null)
+                baseQuery = baseQuery.Where(p => p.Name.Contains(searchTerm)
+                          || p.SKU.Contains(searchTerm));
+            var count = await baseQuery.CountAsync();
+            var pagination = new Pagination<ProductListDto>(count, pageSize, page);
+
+            var products = await baseQuery.OrderBy(p => p.Name)
+                .Skip((pagination.pageNumber - 1) * pagination.pageSize).Take(pagination.pageSize).
+                Select(p => new ProductListDto
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    CategoryName = p.Category.Name,
+                    SKU = p.SKU,
+                    IsActive = p.IsActive,
+                    LastUpdate = p.UpdatedAt == null ? p.CreatedAt : p.UpdatedAt.Value , 
+                    Variants = p.Variants.Select(v => new ProductVariantListDto
+                    {
+                        Id = v.Id,
+                        Color = v.Color.Code,
+                        Size = v.Size.Code,
+                        StockQuantity = v.StockQuantity,
+                        Price = v.SellingPrice,
+                        Sku = v.VariantSKU 
+                    })
+                }).ToListAsync();
+
+            pagination.Items = products;
+            return Result<Pagination<ProductListDto>>.Success(pagination);
         }
     }
 }
