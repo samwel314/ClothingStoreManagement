639b4359c3f1d7ea08e0f5cfc226a7dce3970b55
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-05-01 02:09:13 +0300

    feat : Impelement product Detials Ui

---
+++ ClothingStoreManagement.Application/DTO/UpdateProductBasicDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClothingStoreManagement.Application.DTO
+{
+    public class UpdateProductBasicDto
+    {
+        public Guid ProductId { get; set; }
+
+        [Required(ErrorMessage = "اسم المنتج مطلوب")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "اسم المنتج يجب أن يكون بين 2 و 100 حرف")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = " اختار الفئة ")]
+        public int CategoryId { get; set; }
+    }
+}
+
