18ba8414f46f15902b04fbadec4a64fd943817fa
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-05-03 18:23:10 +0300

    feat : implement invocie models

---
+++ ClothingStoreManagement.Domain/Entities/Invoice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothingStoreManagement.Domain.Entities
+{
+    public class Invoice
+    {
+        public int Id { get; private set; }
+        public string Serial { get; private set; } = null!; // = INV-year-month-day-Id 
+        public DateTime CreatedAt { get; private set; } = DateTime.Now; 
+        public DateTime LastUpdatedAt { get; private set; } // لو غيرنا الحالة بتتغير
+        public decimal TotalAmount { get; private set; } // مجموع سعر البيع لكل الأصناف في الفاتورة
+        public InvoiceStatus Status { get; private set; } = InvoiceStatus.pending;
+        public void CalcTotal( decimal price , int quantity , decimal discount , decimal oldAmount) // لما نضيف صنف جديد أو نعدل صنف موجود في الفاتورة لازم نعيد حساب المجموع
+        {
+            TotalAmount = TotalAmount - oldAmount + (price * quantity) * (1  - discount ) ;  
+        }
+        public void GenerateSerial()
+        {
+            if (string.IsNullOrEmpty(Serial))
+            {
+                Serial = $"INV-{Guid.NewGuid().ToString().Substring(0, 8).ToUpper()}";
+            }
+        }
+        public void UpdateStatus(InvoiceStatus newStatus)
+         {
+             if (Status == InvoiceStatus.returned)
+                 throw new InvalidOperationException("Can't change status of a returned invoice.");
+             if (Status == InvoiceStatus.completed && newStatus == InvoiceStatus.pending)
+                 throw new InvalidOperationException("Can't revert a completed invoice to pending.");
+             Status = newStatus;
+             LastUpdatedAt = DateTime.Now;
+        }
+        public IEnumerable<InvoiceItem> Items { get; private set; } = new List<InvoiceItem>();
+
+    }
+}
---
+++ ClothingStoreManagement.Domain/Entities/InvoiceItem.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClothingStoreManagement.Domain.Entities
+{
+    public class InvoiceItem
+    {
+        public InvoiceItem(int invoiceId, Guid productVariantId, 
+            int quantity, decimal sellingPrice, decimal purchasePrice, decimal discount = 0 )
+        {
+            ValidateStockQuantity(quantity);
+            ValidateSellingPrice(sellingPrice);
+            ValidatePurchasePrice(purchasePrice);
+            InvoiceId = invoiceId;
+            ProductVariantId = productVariantId;
+            Quantity = quantity;
+            SellingPrice = sellingPrice;
+            PurchasePrice = purchasePrice;
+            Discount = discount;
+        }
+        public int Id { get; private set; } 
+        public int InvoiceId { get; private set; }
+        public Guid ProductVariantId { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal SellingPrice { get; private set; }
+        public decimal PurchasePrice { get; private set; }
+        public decimal Discount { get; private set; }
+        public decimal TotalPrice => Quantity * SellingPrice * (1 - Discount);
+        private void ValidateSellingPrice(decimal sellingPrice)
+        {
+            if (sellingPrice <= 0)
+                throw new ArgumentException("sellingPrice must be greater than zero ");
+        }
+        private void ValidatePurchasePrice(decimal purchasePrice)
+        {
+            if (purchasePrice <= 0)
+                throw new ArgumentException("purchasePrice must be greater than zero ");
+        }
+        private void ValidateStockQuantity(int quantity)
+        {
+            if (quantity < 1)
+                throw new ArgumentException(" quantity must be at least 1.");
+        }
+        // nav 
+        [ForeignKey("ProductVariantId")]
+        public ProductVariant ProductVariant { get; private set; } = null!;
+        [ForeignKey("InvoiceId")]
+        public Invoice Invoice { get; private set; } = null!;
+    }
+}
