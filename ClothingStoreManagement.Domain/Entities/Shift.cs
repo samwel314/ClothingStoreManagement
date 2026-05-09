2e1e3ee5be0c61509e0bd39a718cbd432b59381e
Samuel Marzouk <Samwelmarzouk3@gmail.com>
2026-05-09 14:49:06 +0300

    feat  implement shift ui

---
+++ ClothingStoreManagement.Domain/Entities/Shift.cs
@@ -0,0 +1,16 @@
+﻿namespace ClothingStoreManagement.Domain.Entities
+{
+    public class Shift
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; } 
+        public DateTime? EndTime { get; set; }   
+
+        public decimal InitialCash { get; set; } 
+
+        public decimal? FinalCashInDrawer { get; set; } 
+        public string? Notes { get; set; }              
+
+        public int UserId { get; set; } 
+    }
+}
